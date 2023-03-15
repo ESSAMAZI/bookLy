8d2c4d3b11970216a6667d943c3e084d2151aab0
EssamEzi <essamaa119@gmail.com>
2023-03-16 01:10:45 +0300

    adding sliding Text  animation to Splash View

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,43 @@
+import 'package:book/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:book/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 
-class SplashViewbody extends StatelessWidget {
+class SplashViewbody extends StatefulWidget {
   const SplashViewbody({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<SplashViewbody> createState() => _SplashViewbodyState();
+}
+
+class _SplashViewbodyState extends State<SplashViewbody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(minutes: 1));
+    slidingAnimation =
+        //انزل النص الى اسفل ورجوعه الى اعلى
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    // //تحديث القيمه
+    // slidingAnimation.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    //انهاء عمل المتحكم عند الخروج من الشاشه
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +47,9 @@ class SplashViewbody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: 4),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        )
+        //تحديث القيمه
+        //تحديث العنصر المحدد فقط
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
