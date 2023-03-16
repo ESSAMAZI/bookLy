eacefdb8449da87c5a2e8f437cdf1b292153fb77
EssamEzi <essamaa119@gmail.com>
2023-03-17 01:39:33 +0300

    Best seller list view item part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:book/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:book/core/utils/assets.dart';
 import 'package:book/core/utils/styles.dart';
 
 import 'package:flutter/material.dart';
@@ -17,14 +18,48 @@ class HomeViewBody extends StatelessWidget {
           //تصميم الاب بار العلوي
           CustomAppBar(),
           // CustomListViewItem(),
+          //تصميم العروض التمرريه
           FeaturedBooksListView(), //تحتاج الى تحديد مساحه الارتفاع
           SizedBox(height: 40),
           Text(
             'Best Seller',
             style: Styles.titleMedium,
-          )
+          ),
+          BestSellerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 120,
+          child: AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
+                  )),
+            ),
+          ),
+        ),
+        Column(
+          children: [],
+        ),
+      ],
     );
   }
 }
