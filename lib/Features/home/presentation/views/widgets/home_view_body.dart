f9e2e5f637a158cbffb00f22fa279754f63cd19a
EssamEzi <essamaa119@gmail.com>
2023-03-17 01:09:57 +0300

    create style file and title to home view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:book/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:book/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:book/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:book/core/utils/styles.dart';
 
 import 'package:flutter/material.dart';
 
@@ -8,32 +9,21 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        //تصميم الاب بار العلوي
-        CustomAppBar(),
-        // CustomListViewItem(),
-        FeaturedBooksListView(), //تحتاج الى تحديد مساحه الارتفاع
-      ],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: FeaturedListViewItem(),
-          );
-        },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          //تصميم الاب بار العلوي
+          CustomAppBar(),
+          // CustomListViewItem(),
+          FeaturedBooksListView(), //تحتاج الى تحديد مساحه الارتفاع
+          SizedBox(height: 40),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          )
+        ],
       ),
     );
   }
