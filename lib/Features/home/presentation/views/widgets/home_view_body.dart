34c27b52c6c38a526e1c0d337e34f2edc3070868
EssamEzi <essamaa119@gmail.com>
2023-03-21 00:04:02 +0300

    create flie Book Details View

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:book/Features/home/presentation/views/best_seller_list_sliver_view.dart';
+import 'package:book/Features/home/presentation/views/widgets/best_seller_list_sliver_view.dart';
 import 'package:book/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book/Features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:book/core/utils/styles.dart';
