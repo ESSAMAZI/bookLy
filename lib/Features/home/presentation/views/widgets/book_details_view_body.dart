165b4cef24419d48393347de517c262e28352acc
EssamEzi <essamaa119@gmail.com>
2023-03-22 01:43:56 +0300

    add title subitile to book details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:book/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:book/Features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:book/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -10,16 +11,28 @@ class BookDetailsViewBody extends StatelessWidget {
     //نعطي قيمه متغيره لصوره لانها مش ثابته
     // علشان تاخذ الطول العرض لكل المقاسات
     var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           const CustomBookDetailsAppBar(),
           //image
+          SizedBox(height: height * .05),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .17),
             child: const CustomBookImage(),
-          )
+          ),
+          const SizedBox(height: 30),
+          Text('The Jungle Book',
+              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 7),
+          Opacity(
+            opacity: .7,
+            child: Text('Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
+          ),
         ],
       ),
     );
