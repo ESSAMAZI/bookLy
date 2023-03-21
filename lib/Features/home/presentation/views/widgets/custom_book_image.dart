37040192291d8d046c3d320ae6c6b29a97260253
EssamEzi <essamaa119@gmail.com>
2023-03-22 01:13:23 +0300

    add custom image to book details view

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,12 +1,15 @@
 import 'package:book/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class FeaturedListViewItem extends StatelessWidget {
-  const FeaturedListViewItem({Key? key}) : super(key: key);
+class CustomBookImage extends StatelessWidget {
+  const CustomBookImage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
+      //لا يوجد بها طول وعرض
+      //aspectRatio حيث يتم تحديد حجم الصوره على اساس الطول العرض
+      // الموجوده فيه
       aspectRatio: 2.7 / 4,
       child: Container(
         decoration: BoxDecoration(
--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:book/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:book/Features/home/presentation/views/widgets/custom_book_image.dart';
 
 import 'package:flutter/material.dart';
 
@@ -14,7 +14,7 @@ class FeaturedBooksListView extends StatelessWidget {
         itemBuilder: (context, index) {
           return const Padding(
             padding: EdgeInsets.symmetric(horizontal: 8),
-            child: FeaturedListViewItem(),
+            child: CustomBookImage(),
           );
         },
       ),
