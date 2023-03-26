78fe5136580ade7c3ef16db4a50be47011ea9815
EssamEzi <essamaa119@gmail.com>
2023-03-27 02:15:42 +0300

    create home repo

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,10 @@
+abstract class HomeRepo {
+  //  تعمل ايهFeatures نحدد
+  // نحدد الشاشة ماذا تعمل
+  //قائمه الجانبيه لكتب -2 القائمه الكتب كامله
+  fetchBestSellerBooks();
+  fetchFeaturedBooks();
+}
+//The repository pattern
+// رح تعمل ايه فقط Features نستخدمه من اجل ان نحدد بشكل صرايح وواضح
+//بدون التدخل في طريقه عملها فقط نحدد
