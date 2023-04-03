ac8da40b6f876344d6bc07252a82e87151a9e639
EssamEzi <essamaa119@gmail.com>
2023-04-04 02:51:43 +0300

    cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({Key? key, required this.imageUrl}) : super(key: key);
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      //لا يوجد بها طول وعرض
-      //aspectRatio حيث يتم تحديد حجم الصوره على اساس الطول العرض
-      // الموجوده فيه
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            // color: Colors.red,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(imageUrl),
-            )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        //لا يوجد بها طول وعرض
+        //aspectRatio حيث يتم تحديد حجم الصوره على اساس الطول العرض
+        // الموجوده فيه
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) {
+            return const Icon(Icons.error_outline_outlined);
+          },
+        ),
       ),
     );
   }
